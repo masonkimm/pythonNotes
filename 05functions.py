67256d8737835ea43be5fc4b07db372b92d5f38e
SungJin Kim <sungjinkimm@gmail.com>
2021-03-02 16:46:29 -0500

    conditionals complete

--- 05functions.py
+++ 05functions.py
@@ -30,8 +30,8 @@ print(num)
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
 
-##getSum = lambda num1, num2 :  num1 + num2
-def getSum(num1, num2): return num1 + num2
+getSum = lambda num1, num2 :  num1 + num2
+# def getSum(num1, num2): return num1 + num2
 
 
 print(getSum(10, 3))
--- 06conditionals.py
+++ 06conditionals.py
@@ -1,19 +1,49 @@
-# If/ Else conditions are used to decide to do something based on something being true or false
+## If/ Else conditions are used to decide to do something based on something being true or false
+
+x = 3
+y = 11
+
+## Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
+
+# if x > y:
+#     print(f'x:{x} is greater than y:{y}')
+# elif x == y:
+#     print(f'x:{x} is equal to y:{y}')
+# else:
+#     print(f'x:{x} is not greater than y:{y}')
+
+# Nested if
+# if x > 2:
+# 	if x <= 10:
+# 		print(f"x:{x} is greater than 2 but less than or equal to 10")
 
 
+## Logical operators (and, or, not) - Used to combine conditional statements
 
-# Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
-
-
-
-# Logical operators (and, or, not) - Used to combine conditional statements
-
-
-
-
-# Membership Operators (in, not in) - Membership operators are used to test if a sequence is presented in an object
-
-
+# # and
+# if x > 2 and x <= 10:
+# 	print(f"x:{x} is greater than 2 but less than or equal to 10")
+# # or
+# if x > 2 or x < 3:
+# 	print(f"x:{x} is greater than 2 or x: {x} is less than 10")
+# # not
+# if not(x == y):
+# 	print(f'x:{x} is not equal to y:{y}')
 
 
-# Identity Operators (is, is not) - Compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
+## Membership Operators (in, not in) - Membership operators are used to test if a sequence is presented in an object
+numbers = [1, 2, 3, 4, 5]
+
+# if x in numbers:
+# 	print(x in numbers)
+# 	# >> True
+
+# if y not in numbers:
+# 	print(y not in numbers)
+# 	# >> True
+
+## Identity Operators (is, is not) - Compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
+
+# is
+if x is y:
+	print (x is y)
